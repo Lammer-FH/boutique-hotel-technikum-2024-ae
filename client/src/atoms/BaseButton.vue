28a0973ba46af36a052f721c26c1ecf9ae7f8ec3
AntonijeStefanovic <a.stefanovic@DT-StefanovicAntonije>
2024-05-25 22:14:48 +0200

    Initial Vue setup

---
+++ client/src/atoms/BaseButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  background-color: #007bff;
+  color: white;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
---
+++ client/src/pages/HomePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Home Page</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <div>
+        <h1>Welcome to the Boutique Hotel Technikum!</h1>
+        <BaseButton text="Click Me" />
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import BaseButton from '../atoms/BaseButton.vue';
+
+export default {
+  name: 'HomePage',
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    BaseButton,
+  },
+};
+</script>
+
+<style scoped>
+</style>
