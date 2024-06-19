a921f52653c371d33c810d16c629295b6f0f7968
StroSte <s.stropp@gmx.at>
2024-06-19 08:46:45 +0200

    Adding ContactDetails and GoogleMaps to BookingConfirmationPage.vue

--- client/src/pages/BookingConfirmationPage.vue
+++ client/src/pages/BookingConfirmationPage.vue
@@ -3,40 +3,39 @@
     <ion-content>
       <ion-card>
         <ion-card-content>
-          <div class="container"><b>Buchungsnummer: {{ bookingStore.currentBooking.bookingNumber }}</b></div>
           <ion-item v-for="room in this.bookingStore.currentBooking.rooms" :key="room" role="article">
             <RoomCard :room="room"/>
           </ion-item>
-          <BookingFormInput
-              label="Vorname"
-              placeholder="Max"
-              type="text"
-              v-model="bookingStore.currentBooking.guests[0].firstName"
-              errorText="Der Vorname darf nicht länger als 50 Zeichen sein!"
-              :disabled="true"
-          />
-          <BookingFormInput
-              label="Nachname"
-              placeholder="Mustermann"
-              type="text"
-              v-model="bookingStore.currentBooking.guests[0].lastName"
-              errorText="Der Nachname darf nicht länger als 50 Zeichen sein!"
-              :disabled="true"
-          />
-          <BookingFormInput
-              label="Email"
-              placeholder="max.mustermann@mail.com"
-              type="email"
-              v-model="bookingStore.currentBooking.guests[0].email"
-              errorText="Die Email muss gütlig und darf nicht länger als 50 Zeichen sein!"
-              :disabled="true"
-          />
-          <ToggleComponent
-              label="Frühstück"
-              :checked="bookingStore.currentBooking.breakfast"
-              @updateToggle="onUpdateBreakfast"
-              :disabled="true"
-          />
+          <div class="container"><b>Buchungsnummer: {{ bookingStore.currentBooking.bookingNumber }}</b></div>
+          <ion-item>Vorname: {{bookingStore.currentBooking.guests[0].firstName}}</ion-item>
+          <ion-item>Nachname: {{bookingStore.currentBooking.guests[0].lastName}}</ion-item>
+          <ion-item>Email: {{bookingStore.currentBooking.guests[0].email}}</ion-item>
+          <ion-item>Frühstück: {{bookingStore.currentBooking.breakfast ? "Ja" : "Nein"}}</ion-item>
+        </ion-card-content>
+        <ion-card-content>
+          <div class="container"><b>So erreichst du uns:</b></div>
+          <div>
+            <ion-item><h2><HotelName /></h2></ion-item>
+            <ion-item>
+              <AddressComponent />
+            </ion-item>
+            <ion-item>
+              <PhoneComponent />
+            </ion-item>
+            <ion-item>
+              <a href="mailto:office@hotel-technikum.com"><MailComponent /></a>
+            </ion-item>
+          </div>
+          <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2657.269324355657!2d16.374597976819672!3d48.23994274402854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d064f6ed971cf%3A0x99fec738bbb12f9d!2sTechnikum%20Wien!5e0!3m2!1sde!2sat!4v1718778726633!5m2!1sde!2sat"
+                    width="300"
+                    height="250"
+                    style="border:0;"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+          </div>
         </ion-card-content>
       </ion-card>
     </ion-content>
@@ -47,12 +46,14 @@
 import {IonContent, IonItem, IonPage} from "@ionic/vue";
 import {useBookingStore} from "@/store";
 import {RoomCard} from "@/molecules";
-import {BookingFormInput, ToggleComponent} from "@/atoms";
+import {AddressComponent, HotelName, MailComponent, PhoneComponent} from "@/assets";
 
 export default {
   name: "BookingConfirmationPage",
   components: {
-    ToggleComponent, BookingFormInput,
+    MailComponent,
+    PhoneComponent, AddressComponent,
+    HotelName,
     RoomCard, IonItem,
     IonPage,
     IonContent,
